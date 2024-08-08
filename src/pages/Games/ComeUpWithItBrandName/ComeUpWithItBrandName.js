801c270ffe75b6d9bb261688f78eeb462451e179
Artem <artem.dubovov@bk.ru>
2024-08-08 13:23:40 +0300

    feat: добавлена первая страница игры

--- src/pages/Games/ComeUpWithItBrandName/ComeUpWithItBrandName.js
+++ src/pages/Games/ComeUpWithItBrandName/ComeUpWithItBrandName.js
@@ -2,11 +2,21 @@ import Header from '../../../components/Header/Header';
 import Footer from '../../../components/Footer/Footer';
 
 import './style.css';
+import { useState } from 'react';
 
 function ComeUpWithItBrandName() {
+  const [countPage, setCountPage] = useState(0);
   return (
-    <div className="ComeUpWithItBrandName">
+    <div className="comeUpWithItBrandName">
       <Header title='Придумай имя бренда'></Header>
+        <div className='gameWindow'>
+            {countPage === 0 ? 
+                <div>
+                    <h1 className='comeUpWithItBrandName_title'>Есть такая модель для постановки конкретных целей — SMART</h1>
+                    <button className='comeUpWithItBrandName_button' onClick={() => setCountPage(1)}>Вперед</button>
+                </div>
+            : <></>}
+        </div>
       <Footer index={2}></Footer>
     </div>
   );
