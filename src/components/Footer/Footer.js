89ec85de318a63dcda572bda2c512a54d837d701
Artem <artem.dubovov@bk.ru>
2024-08-08 18:41:18 +0300

    feat: добавлены картинки для меню

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
 import './style.css';
-import ic0A from './assets/index-0-active.png';
+import ic0 from './assets/index-0.png';
 import ic1 from './assets/index-1.png';
 import ic2 from './assets/index-2.png';
 import ic3 from './assets/index-3.png';
+import ic0a from './assets/index-0-active.png';
+import ic1a from './assets/index-1-active.png';
+import ic2a from './assets/index-2-active.png';
+import ic3a from './assets/index-3-active.png';
 
 import {data} from './data.js';
-
-const listIcons = [ic0A, ic1, ic2, ic3];
+const listIconsActive = [ic0a, ic1a, ic2a, ic3a];
+const listIcons = [ic0, ic1, ic2, ic3];
 
 console.log(data);
 
@@ -20,7 +24,7 @@ function Footer({index}) {
               data.map((el, i) => 
                 <li key={el.index}> 
                   <Link className={el.index === index ? 'footer_button footer_button--active' : 'footer_button'} to={el.path}>
-                    <img src={listIcons[i]} alt=""></img>
+                    <img src={index === i ? listIconsActive[index] : listIcons[i]} alt=""></img>
                   </Link>
                 </li>
               )
