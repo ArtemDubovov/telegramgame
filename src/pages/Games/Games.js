839eebdc6048538f5d35dbbff52c8dc67e2a0f24
Artem <artem.dubovov@bk.ru>
2024-08-09 14:00:24 +0300

    feat: добавлены картинки на страницу игры

--- src/pages/Games/Games.js
+++ src/pages/Games/Games.js
@@ -4,6 +4,12 @@ import Footer from '../../components/Footer/Footer';
 import { Link } from 'react-router-dom';
 
 import './style.css';
+import triangle from './assets/triangle.svg';
+import wizdef from './assets/wiz-def.svg';
+import wizhandup from './assets/wiz-handup.svg';
+import wizpower from './assets/wiz-power.svg';
+import wizthinking from './assets/wiz-thinking.svg';
+
 
 function Games() {
   return (
@@ -13,25 +19,41 @@ function Games() {
         <h1 className='games-title'>
           Все 👋 курсы  
           🐊 Брендодила
-          бесплатно 
+          бес<span className='text_through'>платно</span> 
         </h1>
       </section>
       <section className='games_block games_block--yellow'>
         <h2 className='games_block_title'>Создай конкурентное 
         позиционирование</h2>
+        <img className='games_block_wizhandup' src={wizhandup} alt="wiz"/>
+        <div className='block_play'>
+          <img src={triangle}/>
+        </div>
       </section>
       <section className='games_block games_block--green'>
         <div className='games_block_info'>
           <h2 className='games_block_title'>Поставь цель по SMART</h2>
         </div>
+        <img className='games_block_wizdef' src={wizdef} alt="wiz"/>
+        <div className='block_play'>
+          <img src={triangle}/>
+        </div>
       </section>
       <section className='games_container'>
         <div className='games_block_big'>
           <h2 className='games_block_title'>Разработай сильное УТП</h2>
+          <img className='games_block_wizpower' src={wizpower} alt="wiz"/>
+          <div className='block_play'>
+            <img src={triangle}/>
+          </div>
         </div>
-        <Link to={'/ComeUpWithItBrandName'} className='games_block_big'>
+        <Link to={'/ComeUpWithItBrandName'} className='games_block_big games_block_big--green'>
           <h2 className='games_block_title'>Придумай
           имя бренда</h2>
+          <img className='games_block_wizthinking' src={wizthinking} alt="wiz"/>
+          <div className='block_play'>
+            <img src={triangle}/>
+          </div>
         </Link>
       </section>
       <Footer index={2}></Footer>
